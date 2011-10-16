e3cec3bf0b8e5134151d9b6debf461c6d8a6ca98
Sam Olsen <olsensh@gmail.com>
2011-10-16 14:37:06 -0700

    Initial Commit

---
+++ SOSelectionView/SOAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SOAppDelegate.h
+//  SOSelectionView
+//
+//  Created by Sam Olsen on 10/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SOViewController;
+
+@interface SOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) SOViewController *viewController;
+
+@end
---
+++ SOSelectionView/SOViewController.h
@@ -0,0 +1,13 @@
+//
+//  SOViewController.h
+//  SOSelectionView
+//
+//  Created by Sam Olsen on 10/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SOViewController : UIViewController
+
+@end
